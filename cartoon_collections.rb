8f731f0b43b8409005ccfb031945cc145f6e63e6
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 12:08:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.find {|i| i.length > 4}
+  array.include? {|i| i.length > 4} 
 end
 
 def find_the_cheese# code an argument here
